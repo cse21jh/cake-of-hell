34575cfd05eac57bef0b2cf2f546772b33eca159
신어진 <doubleslit1801@gmail.com>
2022-07-19 19:19:25 +0900

    Add Util class
    Util 클래스의 모든 함수는 static이므로 Util.함수명()을 통해 실행할 수 있습니다.
    GetItem: 아이템 코드로 아이템을 불러오는 함수입니다.
    CountItem: 아이템 코드에 맞는 아이템의 개수를 세 주는 함수입니다.
    UseItem: 아이템 코드와 사용할 아이템 개수를 받아 아이템을 사용하는 함수입니다. 사용할 아이템의 개수는 현재 있는 아이템 개수보다 많을 수 없습니다. count를 명시하지 않으면 1로 처리됩니다.
    AddItem: 아이템 코드와 증가할 아이템 개수를 받아 아이템 개수를 증가시키는 함수입니다.

---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    public static Item GetItem(int code)
+    {
+        return ItemManager.Instance.GetItem(code);
+    }
+
+    public static int CountItem(int code) 
+    {
+        return SaveManager.Instance.GetNumberOfItem(code);
+    }
+
+    public static void UseItem(int code, int count = 1)
+    {
+        if(CountItem(code) >= count) 
+        {
+            SaveManager.Instance.SetNumberOfItem(code, CountItem(code) - count);
+        }
+        else
+        {
+            throw new System.ArgumentOutOfRangeException("count", System.String.Format("현재 아이템 개수인 {0}개보다 많은 {1}개의 아이템을 소모할 수 없습니다.", CountItem(code), count));
+        }
+    }
+
+    public static void AddItem(int code, int count = 1)
+    {
+        SaveManager.Instance.SetNumberOfItem(code, CountItem(code) + count);
+    }
+}
