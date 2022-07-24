9c85be06e27a1f9ca5ac7f6c73e7fdb49beb8f0a
RYZEN-PC\RYZEN <jjun0403@korea.ac.kr>
2022-07-25 03:02:18 +0900

    Add Sound,Font Asset

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+
+    private AudioSource BgmPlayer;
+    private AudioSource EffectPlayer;
+
+    [SerializeField] private AudioClip[] EffectAudioClips;
+
+    Dictionary<string, AudioClip> EffectSoundDictionary = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        GameObject EffectTempObject = new GameObject("Effect");
+        EffectTempObject.transform.SetParent(transform);
+        EffectPlayer = EffectTempObject.AddComponent<AudioSource>();
+
+        GameObject BgmTempObject = new GameObject("Bgm");
+        BgmTempObject.transform.SetParent(transform);
+        BgmPlayer = BgmTempObject.AddComponent<AudioSource>();
+
+        foreach (AudioClip audioclip in EffectAudioClips)
+        {
+            EffectSoundDictionary.Add(audioclip.name, audioclip);
+        }
+    }
+
+    public void PlayEffect(string name, float volume = 1f)
+    {
+        EffectPlayer.PlayOneShot(EffectSoundDictionary[name], volume);
+    }
+
+    public void PlayBgm(AudioClip clip, float volume = 1f)
+    {
+        BgmPlayer.loop = true;
+        BgmPlayer.volume = volume;
+
+        BgmPlayer.clip = clip;
+        BgmPlayer.Play();
+    }
+
+    public void StopBgm()
+    {
+        BgmPlayer.clip = null;
+        BgmPlayer.Stop();
+    }
+
+    public void ChangeBgmVolume(float volume)
+    {
+        BgmPlayer.volume = volume;
+    }
+    
+
+}
