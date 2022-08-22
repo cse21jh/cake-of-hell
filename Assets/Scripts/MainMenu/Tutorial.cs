dae63c5a32a985c00264242dd0926c876caade68
Yoon <cse21jh@korea.ac.kr>
2022-08-23 05:26:59 +0900

    Build up for tutorial
    Henaga sajin jalmot jouhseo wansungbon ollil su upsseom

---
+++ Assets/Scripts/MainMenu/Tutorial.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    private Sprite[] tutorialBackgroundArr;
+    private int num = 0;
+
+    public ClickUI[] ScreenTouch;
+    public GameObject TutorialScreen;
+
+
+    void Start()
+    {
+        tutorialBackgroundArr = ResourceLoader.GetPackedSprite("Sprites/Background/tutorial");
+        ShowTutorial();
+    }
+
+    private void ShowTutorial()
+    {
+        ScreenTouch[0].AddListenerOnly(() =>
+        {
+            if (num > 0) num -= 1;
+            ShowNextBackground(num);
+        });
+        ScreenTouch[1].AddListenerOnly(() =>
+        {
+            //if (num == tutorialBackgroundArr.Length - 1) SceneManager.LoadScene("StartScene");
+            if (num < tutorialBackgroundArr.Length - 1) num += 1;
+            ShowNextBackground(num);
+        });
+    }
+
+    private void ShowNextBackground(int n)
+    {
+        TutorialScreen.GetComponent<Image>().sprite = tutorialBackgroundArr[n];
+        ShowTutorial();
+    }
+}
